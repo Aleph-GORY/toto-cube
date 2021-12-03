6eba49394141d5f41ce743f94c57146ede1f4685
Armando Cruz (Gory) <aleph_g@ciencias.unam.mx>
2021-12-03 09:31:38 -0600

    feat: optimized search

---
+++ block.js
@@ -0,0 +1,24 @@
+class Steve {
+    orientations = [
+        [3, 2, 2],
+        [2, 3, 2],
+        [2, 2, 3]
+    ];
+}
+
+class Carmen {
+    orientations = [
+        [1, 2, 4],
+        [1, 4, 2],
+        [2, 1, 4],
+        [2, 4, 1],
+        [4, 2, 1],
+        [4, 1, 2]
+    ];
+}
+
+class Unit {
+    orientations = [
+        [1, 1, 1]
+    ]
+}
